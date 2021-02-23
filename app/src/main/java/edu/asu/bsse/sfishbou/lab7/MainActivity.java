986652cdf9ce246a35eb530450084f1763206f98
sfish97 <sfishbough@gmail.com>
2021-02-23 13:02:41 -0800

    Basic Action Bar

--- app/src/main/java/edu/asu/bsse/sfishbou/lab7/MainActivity.java
+++ app/src/main/java/edu/asu/bsse/sfishbou/lab7/MainActivity.java
@@ -2,13 +2,49 @@ package edu.asu.bsse.sfishbou.lab7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.AdapterView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        android.util.Log.d(this.getClass().getSimpleName(), "called onCreateOptionsMenu()");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                android.util.Log.d(this.getClass().getSimpleName(), "Setting clicked");
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                android.util.Log.d(this.getClass().getSimpleName(), "Fav clicked");
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
