7ed27721fa576378b8e97a7e1e49c76e09c1316c
sfish97 <sfishbough@gmail.com>
2021-03-03 02:23:47 -0800

    Clean up

--- app/src/main/java/edu/asu/bsse/sfishbou/lab7/PlacesDB.java
+++ app/src/main/java/edu/asu/bsse/sfishbou/lab7/PlacesDB.java
@@ -6,9 +6,6 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteStatement;
-
-import androidx.annotation.Nullable;
 
 import java.io.File;
 import java.io.FileOutputStream;
