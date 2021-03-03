e299f8f1ab62adfe922c4ba4204d134b53926d82
sfish97 <sfishbough@gmail.com>
2021-03-03 02:23:17 -0800

    Added database, initalize database SQL file, PlacesDB class that handles Database open/closing

---
+++ app/src/main/res/raw/init_places_db.sql
@@ -0,0 +1,35 @@
+DROP TABLE places;
+DROP TABLE placeDescription;
+
+CREATE TABLE places(
+	name TEXT PRIMARY KEY
+);
+
+CREATE TABLE placeDescription(
+	name TEXT,
+    description TEXT,
+    category TEXT,
+    addressTitle TEXT,
+    addressStreet TEXT,
+    elevation FLOAT,
+    latitude FLOAT,
+	longitude FLOAT,
+    FOREIGN KEY(name) REFERENCES places(name) ON DELETE CASCADE
+);
+
+
+INSERT INTO places VALUES
+   ('ASU-Poly');
+   
+INSERT INTO placeDescription VALUES
+   (
+   'ASU-Poly',
+   "Home of ASU's Software Engineering Programs",
+   'School',
+   'ASU Software Engineering',
+   '7171 E Sonoran Arroyo Mall\nPeralta Hall 230\nMesa AZ 85212',
+   1384.0,
+   33.306388,
+   -111.679121
+   );
+
