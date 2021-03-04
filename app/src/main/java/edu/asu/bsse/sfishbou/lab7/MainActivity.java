a2dcbfcde8415a7a287a993aeacc7ef23491049a
sfish97 <sfishbough@gmail.com>
2021-03-03 23:59:13 -0800

    Added AddPlaceActivity screen

--- app/src/main/java/edu/asu/bsse/sfishbou/lab7/MainActivity.java
+++ app/src/main/java/edu/asu/bsse/sfishbou/lab7/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -70,7 +71,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             case R.id.action_addPlace:
                 // User chose the "Settings" item, show the app settings UI...
                 android.util.Log.d(this.getClass().getSimpleName(), "Add clicked");
-                //Intent intent = new Intent(this, AddPlaceActivity.class);
+                Intent intent = new Intent(this, AddPlaceActivity.class);
+                startActivity(intent);
                 return true;
 
             case R.id.action_modifyPlace:
